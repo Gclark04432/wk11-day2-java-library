8b8bb1502c964c2bb0dde9b6ff6940875a4bf1cb
Gary Clark <gclark04432@live.co.uk>
2020-02-11 19:26:31 +0000

    add book class and test file with set uo

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,10 +1,28 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class BookTest {
     private Book book;
 
     @Before
     public void before(){
-        book = new Book();
+        book = new Book("Ulysees", "James Joyce", "Modernist");
+    }
+
+    @Test
+    public void canGetTitle(){
+        assertEquals("Ulysees", book.getTitle());
+    }
+
+    @Test
+    public void canGetAuthor(){
+        assertEquals("James Joyce", book.getAuthor());
+    }
+
+    @Test
+    public void canGetGenre(){
+        assertEquals("Modernist", book.getGenre());
     }
 }
