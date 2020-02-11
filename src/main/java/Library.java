a94682702b655d90a5dbe86de5dbf0735137f6f6
Gary Clark <gclark04432@live.co.uk>
2020-02-11 19:30:58 +0000

    create library class and test and setup

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> books;
+
+    public Library(){
+        this.books = new ArrayList<Book>();
+    }
+
+    public int booksCount() {
+        return this.books.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class LibraryTest {
+    private Library library;
+
+    @Before
+    public void before() {
+        library = new Library();
+    }
+
+    @Test
+    public void booksStartsEmpty() {
+        assertEquals(0, library.booksCount());
+    }
 }
