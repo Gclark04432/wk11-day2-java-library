8d68963e8c6202bc0fbc7a7400fb6656782c91a4
Gary Clark <gclark04432@live.co.uk>
2020-02-11 19:40:18 +0000

    add conditional logic to only add book if space available

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,12 +2,21 @@ import java.util.ArrayList;
 
 public class Library {
     private ArrayList<Book> books;
+    private int capacity;
 
     public Library(){
         this.books = new ArrayList<Book>();
+        this.capacity = 3;
     }
 
     public int booksCount() {
         return this.books.size();
+    }
+
+    public void add(Book book) {
+        if (this.booksCount() < this.capacity) {
+            this.books.add(book);
+        }
+        else System.out.println("No space available for book");
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -7,14 +7,31 @@ import static junit.framework.TestCase.assertEquals;
 
 public class LibraryTest {
     private Library library;
+    private Book book;
 
     @Before
     public void before() {
         library = new Library();
+        book = new Book("Ulysees", "James Joyce", "Modernist");
     }
 
     @Test
     public void booksStartsEmpty() {
         assertEquals(0, library.booksCount());
+    }
+
+    @Test
+    public void canAddBook() {
+        library.add(book);
+        assertEquals(1, library.booksCount());
+    }
+
+    @Test
+    public void canOnlyAddBookIfSpaceAvailable() {
+        library.add(book);
+        library.add(book);
+        library.add(book);
+        library.add(book);
+        assertEquals(3, library.booksCount());
     }
 }
